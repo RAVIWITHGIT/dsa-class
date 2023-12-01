9545cc5fd59fced7911b5a5a97ec634927a319b5
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-01 18:34:49 +0530

    update my code

---
+++ tut8/task.js
@@ -0,0 +1,33 @@
+// let arr = [2,5,6,1,8,9,3,10]
+
+
+// for (let i = 0; i < arr.length; i++) {
+//   m= Math.floor(arr.length/2)
+//   if(i==m){
+//     break
+    
+//   }
+//   let temp = arr[i]
+//   arr[i]=arr[arr.length-1-i]
+//   arr[arr.length-1-i]= temp
+
+  
+  
+// }
+// console.log(arr)
+
+
+// let arr1 = [2,5,6,1,8,9,3,10,11]
+
+// let m1 = arr1.length/2
+
+// let i = 0
+// while(i<m1){
+//     temp = arr1[i]
+//     arr1[i]= arr1[arr1.length-1-i]
+//     arr1[arr1.length-1-i] = temp
+//     i++
+
+// }
+// console.log(arr1)
+
