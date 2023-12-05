36ec57e8f347f99b3128e819d5d9e97dc1c9f303
ravi lakshakar <ravilakshakar336@gmail.com>
2023-12-05 16:14:20 +0530

    update code

---
+++ tut9/main.js
@@ -0,0 +1,27 @@
+let arr = [1,2,3,4,5,6]
+let narr = []
+let f = 0
+for (let i =arr.length-1; i >=0; i--) {
+    narr[f] = arr[i]
+    f++
+
+    
+}
+console.log(narr)
+
+
+
+let str = 'ravi'
+let nstr = ''
+let s = 0
+for (let i = str.length-1; i >= 0; i--) {
+
+
+
+    
+}
+console.log(nstr)
+
+
+
+
